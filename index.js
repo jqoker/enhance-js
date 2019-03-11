d9c176ffb02d6ab32b818047d4612f8460f67231
hongliang.yu <45158982+jqoker@users.noreply.github.com>
2019-03-11 16:44:01 +0800

    Update index.js
    添加注释。

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 /**
- * 增强
- * @param  {Array}  [funcs=[]]       [description]
- * @param  {Number} [initialValue=1] [description]
- * @return {[type]}                  [description]
+ * 递归增强
+ * @param  {Array}  [funcs=[]]       [待增强函数集]
+ * @param  {Number} [initialValue=1] [初始值]
+ * @return {[type]}                  [增强后的值]
  */
 export const pipe = (funcs = [], initialValue = 1) => funcs.reduce((a, b) => b(a), initialValue);
 
 /**
  * 合并增强
- * @param  {Array}  [funcs=[]]       [待合并函数]
+ * @param  {Array}  [funcs=[]]       [待增强函数集]
  * @param  {Number} [initialValue=1] [初始值]
- * @return {[type]}                  [合并增强后的值]
+ * @return {[type]}                  [增强后的值]
  */
 export const combine = (funcs = [], initialValue = 1) => funcs.reduce((a, b) => a * b(initialValue), initialValue);
